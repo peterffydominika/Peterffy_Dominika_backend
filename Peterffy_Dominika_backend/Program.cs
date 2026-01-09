9578bdfd310a1de974bc3d9330d1afbb77bb3b8e
peterffydominika <peterffy.dominika@gmail.com>
2026-01-09 09:53:58 +0100

    14.feladat is ready.

--- Peterffy_Dominika_backend/Program.cs
+++ Peterffy_Dominika_backend/Program.cs
@@ -1,3 +1,5 @@
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Options;
 using Peterffy_Dominika_backend.Models;
 using Peterffy_Dominika_backend.Models.DTOs;
 using Peterffy_Dominika_backend.Services;
@@ -24,12 +26,14 @@ namespace Peterffy_Dominika_backend
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            builder.Services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());});
+
 
             // Store the required UID in configuration so controllers can compare against it
             builder.Configuration["UID"] = "FKB3F4FEA09CE43C";
 
             var app = builder.Build();
-
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
